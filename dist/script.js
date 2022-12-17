a986fe36f601a45ffca7d9bc23e3d3f81a61a944
nat-davydova <natti.davydova@gmail.com>
2022-12-17 14:45:20 +0300

    feat: add horz direction detection

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,21 @@
 const tile1 = document.querySelector(".tiles-nav__item");
 
-tile1.addEventListener("mouseenter", () => {
-  console.log("hey");
+tile1.addEventListener("mouseenter", (event) => {
+  const {
+    left: tileXLeft,
+    right: tileXRight,
+    top: tileYTop,
+    bottom: tileYBottom,
+    width: tileWidth,
+    height: tileWidthHeight
+  } = tile1.getBoundingClientRect();
+
+  const cursorX = event.clientX;
+  const cursorY = event.clientY;
+
+  const horzDirection = cursorX < tileWidth / 2 ? "left" : "right";
+
+  console.log({ cursorX, cursorY });
+  console.log({ tileXLeft, tileXRight, tileYTop, tileYBottom });
+  console.log(horzDirection);
 });
