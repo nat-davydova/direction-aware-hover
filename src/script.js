09a4dfd7aa76b5ef158ff1fee7e182d90c38fcd4
nat-davydova <natti.davydova@gmail.com>
2022-12-17 16:36:29 +0300

    feat: make mousemove trigger only once while hovered

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 const tile = document.querySelectorAll(".tiles-nav__item")[1];
 
-tile.addEventListener("mouseenter", (event) => {
+let mouseEnterFlag = false;
+
+tile.addEventListener("mousemove", (event) => {
+  if (mouseEnterFlag) {
+    return;
+  }
+
   const {
     left: tileXLeft,
     right: tileXRight,
@@ -13,13 +19,44 @@ tile.addEventListener("mouseenter", (event) => {
   const cursorX = event.clientX;
   const cursorY = event.clientY;
 
-  const tileCenterX = tileXLeft + (tileXRight - tileXLeft) / 2;
-  const tileCenterY = tileYTop + (tileYBottom - tileYTop) / 2;
+  const isHovered =
+    cursorX >= tileXLeft &&
+    cursorX <= tileXRight &&
+    cursorY >= tileYTop &&
+    cursorY <= tileYBottom;
 
-  const horzDirection = cursorX < tileCenterX ? "left" : "right";
-  const vertDirection = cursorY < tileCenterY ? "top" : "bottom";
+  if (!isHovered) {
+    return;
+  }
+
+  let direction = "";
+
+  if (event.movementY > 0 && event.movementX === 0) {
+    direction = "down";
+  }
+
+  if (event.movementY < 0 && event.movementX === 0) {
+    direction = "up";
+  }
+
+  if (event.movementY === 0 && event.movementX > 0) {
+    direction = "right";
+  }
+
+  if (event.movementY === 0 && event.movementX < 0) {
+    direction = "left";
+  }
+
+  mouseEnterFlag = true;
 
   console.log({ cursorX, cursorY });
-  console.log({ tileXLeft, tileXRight, tileYTop, tileYBottom });
-  console.log(horzDirection, vertDirection);
+  //console.log({ tileXLeft, tileXRight, tileYTop, tileYBottom });
+  console.log(isHovered);
+  console.log({
+    direction,
+    movementX: event.movementX,
+    movementY: event.movementY
+  });
 });
+
+tile.addEventListener("mouseleave", () => (mouseEnterFlag = false));
